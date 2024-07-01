9270fc04ce97c58c7b4068f91d5e439e1d2972fb
notlegos <roger@mustacich.com>
2024-07-01 15:41:38 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -30,6 +30,11 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     radioThrottle("joyButton", 6)
 })
+radio.onReceivedValue(function (name, value) {
+    if (name == "joyVibrate") {
+        joystickbit.Vibration_Motor(value)
+    }
+})
 joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P13, joystickbit.ButtonType.down, function () {
     radioThrottle("joyButton", 2)
 })
@@ -39,6 +44,7 @@ joystickbit.onButtonEvent(joystickbit.JoystickBitPin.P12, joystickbit.ButtonType
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     radioThrottle("joyButton", 8)
 })
+let requestTime = 0
 let isUp = false
 let isLeft = false
 let magnitude = 0
@@ -46,7 +52,6 @@ let isLeftRight = false
 let inDeadzone = false
 let theY = 0
 let theX = 0
-let requestTime = 0
 let throttleLastSend = 0
 let throttleLastNumber = 0
 let throttleLastString = ""
@@ -56,12 +61,12 @@ let deadzone = 20
 throttleLastString = ""
 throttleLastNumber = 0
 throttleLastSend = 0
-basic.forever(function () {
+while (true) {
     theX = joystickbit.getRockerValue(joystickbit.rockerType.X)
     theY = joystickbit.getRockerValue(joystickbit.rockerType.Y)
     inDeadzone = Math.abs(512 - theX) < deadzone && Math.abs(512 - theY) < deadzone
     if (inDeadzone) {
-        basic.pause(0)
+        basic.pause(50)
     } else {
         isLeftRight = Math.abs(512 - theX) > Math.abs(512 - theY)
         if (isLeftRight) {
@@ -77,6 +82,7 @@ basic.forever(function () {
                 }
             }
             radioThrottle("joyCenter", 1)
+            basic.pause(50)
         } else if (!(isLeftRight)) {
             while (!(inDeadzone)) {
                 theY = joystickbit.getRockerValue(joystickbit.rockerType.Y)
@@ -90,6 +96,10 @@ basic.forever(function () {
                 }
             }
             radioThrottle("joyCenter", 1)
+            basic.pause(50)
         }
     }
+}
+basic.forever(function () {
+	
 })
